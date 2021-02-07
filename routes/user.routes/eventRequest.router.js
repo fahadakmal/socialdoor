4959fa2bcb5f6817f5ebde9e3cb77b070f525eb5
fahad_akmal <fahad.akmal@lieferhome.de>
2021-02-07 13:59:49 +0500

    changes for routes

--- routes/user.routes/eventRequest.router.js
+++ routes/user.routes/eventRequest.router.js
@@ -1,2 +1,16 @@
 //mobileUser user will get all user requests for that hosted event  by using this  routes
 router.get('getRequestsForEvent')
+
+
+//mobielUser user can send message to host by using this route
+router.post('/contactHost');
+//mobielUser by using this event request to host for approval for this event 
+router.post('/requestHostForEvent');
+//mobielUser if host will accept the user request
+router.post('/acceptRequest');
+//mobielUser host if host will reject the user request
+router.post('/cancelRequest');
+//mobielUser host  will add a reason for cancelling user request
+router.post('/addReasonForCancel');
+//mobielUser host will get userDetail for his/her id  with comments
+router.post('/getUserByIdForRequest')
--- routes/user.routes/story.router.js
+++ routes/user.routes/story.router.js
@@ -1,2 +1,5 @@
 //mobielUser request to all stories that will be shown on home screen
 router.post('getStories')
+router.post('/addStory')
+//mobielUser will delete a story by using this story
+router.post('deleteStory')  
